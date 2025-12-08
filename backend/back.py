48d38d66b08a2f586c68819f476f432ece6d3513
saimonemon46 <saimonemon46@gmail.com>
2025-12-08 23:54:23 +0600

    Added Continous Chatting Options

--- backend/back.py
+++ backend/back.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from typing import Annotated, TypedDict
 
 from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage
 
 from langgraph.graph import START, END, StateGraph
 
@@ -19,5 +20,21 @@ llm = ChatGroq(
     model_name="openai/gpt-oss-20b"      
 )
 
-## Check llm
-print(llm.invoke("how are you").content)
+# ## Check llm
+# print(llm.invoke("how are you").content)
+
+##### Chat Function ######
+def generate_response(input : str):
+    # user input as HumanMessage
+    messages = [HumanMessage(content=input)]
+    response = llm.invoke(messages)
+    return response.content
+
+##### Test Chat Function ######
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ['exit', 'quit', 'bye']:
+            break
+        response = generate_response(user_input)
+        print("AI:", response)
